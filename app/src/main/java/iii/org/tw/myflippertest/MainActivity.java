417d62451ed98d2d1b7b229f288d74524e2e721f
cf904d31 <cf904d31@gmail.com>
2016-09-22 13:11:18 +0800

    MyFlipperTest(頁面滑動不使用ObjectAnimator)

--- app/src/main/java/iii/org/tw/myflippertest/MainActivity.java
+++ app/src/main/java/iii/org/tw/myflippertest/MainActivity.java
@@ -4,19 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     private ViewFlipper viewflipper;
+    private Animation animInLeftRight, animInRightLeft, animOutLeftRight, animOutRightLeft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        animInLeftRight = AnimationUtils.loadAnimation(this, R.anim.in_leftright);
+        animOutLeftRight = AnimationUtils.loadAnimation(this, R.anim.out_leftright);
+
         viewflipper = (ViewFlipper) findViewById(R.id.viewflipper);
+
+
         viewflipper.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                viewflipper.setInAnimation(animInLeftRight);
+                viewflipper.setOutAnimation(animOutLeftRight);
                 viewflipper.showNext();
                 return false;
             }
